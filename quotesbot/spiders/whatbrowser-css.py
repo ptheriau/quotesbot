076f64f12108506138f124e2dec80ba7a8357682
ptheriau <philippe.theriault@umontreal.ca>
2020-12-11 18:48:38 -0500

    Update whatbrowser-css.py

--- quotesbot/spiders/whatbrowser-css.py
+++ quotesbot/spiders/whatbrowser-css.py
@@ -6,7 +6,15 @@ class ToScrapeCSSSpider(scrapy.Spider):
     start_urls = [
         'https://www.whatsmybrowser.org/',
     ]
+        
+    def parse(self, response):
+        for row in response.css("div.browser-details-table"):
+            yield {
+                'name': row.css("div.name::text").extract_first(),
+                'value': row.css("div.value::text").extract_first(),
+            }
 
+            
     def parse(self, response):
         yield {
                 'useragent': response.css("div.user-agent::text").extract_first(),
