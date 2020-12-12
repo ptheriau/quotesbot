3df7d82898a055aeace9394adc885538558e0658
ptheriau <philippe.theriault@umontreal.ca>
2020-12-11 22:27:07 -0500

    Update iga.py

--- quotesbot/spiders/iga.py
+++ quotesbot/spiders/iga.py
@@ -11,8 +11,8 @@ class IGA_Spider(scrapy.Spider):
     def parse(self, response):
         for product in response.css("div.item-product__content"):
             yield {
-                'brand': product.css("a.item-product__brand::text").extract,
+                'brand': product.css("div.item-product__brand::text").extract_first(),
                 'name': product.css("a.js-ga-productname::text").extract_first(),
                 #'link': product.css('a.js-ga-productname::attr("href")'.extract_first(),
-                'size': product.css("item-product__info::text").extract,
+                'size': product.css("div.item-product__info::text").extract_first(),
             }
