203c8a918f6cbad93760229ff24f36c95eaed292
ptheriau <philippe.theriault@umontreal.ca>
2020-12-12 23:42:01 -0500

    Create metro.py

---
+++ quotesbot/spiders/metro.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+class IGA_Spider(scrapy.Spider):
+    name = "Metro-spider"
+    allowed_domains = ["metro.ca"]
+    start_urls = [
+        'https://www.metro.ca/en/online-grocery/search',
+    ]
+        
+    def parse(self, response):
+        for product in response.css("div.products-tile-list__tile"):
+            
+            promoselector=product.css("div.pi-price-promo::text")
+            if promoselector:
+                tempsaleprice=product.css("div.pi-sale-price .pi-price::text").extract_first().strip()
+                tempregprice=product.css("div.pi-regular-price .pi-price::text").extract_first().strip()
+            else:
+                tempsaleprice=""
+                tempregprice=product.css("span.pi-price::text").extract_first().strip()
+            
+            yield {
+                'brand': product.css("span.pt-brand::text").extract_first().strip(),
+                'name': product.css("div.pt-title::text").extract_first().strip(),
+                'link': product.css("a.product-details-link::attr(href)").extract_first(),
+                'size': product.css("span.pt-weight::text").extract_first().strip(),
+                'regprice': tempregprice,
+                'saleprice': tempsaleprice,
+            }
+            #nextpagelinkselector=response.css(".icon--arrow-skinny-right::attr(href)")
+            #if nextpagelinkselector:
+            #    nextpagelink=nextpagelinkselector[0].extract()
+            #    yield scrapy.Request(url=response.urljoin(nextpagelink))
